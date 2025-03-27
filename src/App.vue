00b0b0f13fb7a601672ae9556b2c91295c622ca8
Cett <florian_steinmann@hotmail.de>
2025-03-27 07:36:48 +0100

    iot funktionen initial hinzugefügt

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,12 @@
 <script setup lang="ts">
 import Todos from './components/Todos.vue'
+import TemperatureDisplay from './components/TemperatureDisplay.vue'
 </script>
 
 <template>
   <main>
     <Todos />
+    <TemperatureDisplay />
   </main>
 </template>
 
