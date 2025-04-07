3c697381260b0ff08ecb96b9f37391a0b7da187c
Cett <florian_steinmann@hotmail.de>
2025-04-07 10:59:09 +0200

    grahana test

--- src/components/temperatureGraph.vue
+++ src/components/temperatureGraph.vue
@@ -10,5 +10,5 @@
 </template>
 
 <script setup>
-const grafanaUrl = "https://g-36c53baaa0.grafana-workspace.eu-central-1.amazonaws.com/d/behlnsryyqpz4b/temperaturedashboard?orgId=1&from=1743530044792&to=1743530344792";
+const grafanaUrl = "https://g-601ffdfc7e.grafana-workspace.eu-central-1.amazonaws.com/dashboard/snapshot/9qm0rBo5mRLlyr7WhJHXwBwn7NqhcC86";
 </script>
