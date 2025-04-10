e889ca9715aa3f6d190032a6a3340096ee0faf41
Cett <florian_steinmann@hotmail.de>
2025-04-10 19:26:32 +0200

    änderungen Graphql Schema

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,7 @@
 <script setup lang="ts">
   import { Authenticator } from "@aws-amplify/ui-vue";
   import "@aws-amplify/ui-vue/styles.css";
-  import temperatureGraph from './components/temperatureGraph.vue'
   import ShowLiveTemperature from "@/components/ShowLiveTemperature.vue";
-  import tempGrafana from "@/components/TemperatureGraph.vue";
   import tempGrafanaApi from "@/components/tempGrafanaAPI.vue";
 </script>
 
@@ -12,9 +10,7 @@
     <authenticator>
       <template v-slot="{ signOut }">
         <ShowLiveTemperature />
-        <!--<temperatureGraph />-->
         <tempGrafanaApi />
-        <!--<tempGrafana />-->
         <button @click="signOut">Sign Out</button>
       </template>
     </authenticator>
